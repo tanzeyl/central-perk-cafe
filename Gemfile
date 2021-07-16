7b28a312be2192bc0c334df2dd1d518820e5bfdf
Tanzeel Khan <ktanzeel80@gmail.com>
2021-07-16 12:30:52 +0530

    Fixing a bug

--- Gemfile
+++ Gemfile
@@ -55,4 +55,3 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "pg", "~> 1.2"
-gem 'invoicing'
--- app/models/invoicing_line_item.rb
+++
@@ -1,5 +0,0 @@
-class InvoicingLineItem < ActiveRecord::Base
-  acts_as_line_item
-
-  belongs_to :ledger_item, class_name: 'InvoicingLedgerItem'
-end
