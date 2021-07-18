df0c19568a537c56d85fdf02b2958d0661a47837
Tanzeel Khan <ktanzeel80@gmail.com>
2021-07-18 11:27:38 +0530

    Fixed a dashboard issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,9 @@ class ApplicationController < ActionController::Base
 
   def current_user
     return @current_user if @current_user
-    current_user_id = session[:current_user_id]
-    if current_user_id
-      @current_user = Customer.find(current_user_id)
+    @current_user_id = session[:current_user_id]
+    if @current_user_id
+      @current_user = Customer.find(@current_user_id)
     else
       nil
     end
