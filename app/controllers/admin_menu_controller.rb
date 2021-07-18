2d07a2a947b9cd6776f8e0abe51d74a2a5dd10ab
Tanzeel Khan <ktanzeel80@gmail.com>
2021-07-18 12:29:08 +0530

    Added edit existing menu feature

--- app/controllers/admin_menu_controller.rb
+++ app/controllers/admin_menu_controller.rb
@@ -1,3 +1,37 @@
 class AdminMenuController < ApplicationController
 
+  def index
+    render "index"
+  end
+
+  def edit_menu
+    render "add_new"
+  end
+
+  def add_items
+    id = params[:id]
+    item = MenuItem.find(id)
+    if item.available == true
+      item.available = false
+      item.save
+    else
+      item.available = true
+      item.save
+    end
+    redirect_to "/edit_menu"
+  end
+
+  def add_cat
+    id = params[:id]
+    item = MenuCategory.find(id)
+    if item.available == true
+      item.available = false
+      item.save
+    else
+      item.available = true
+      item.save
+    end
+    redirect_to "/edit_menu"
+  end
+
 end
